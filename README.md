0e83ebbc049f9030defb937adfffe8a5903a8dd7
gbwlxhd97 <gbwlxhd97@naver.com>
2021-02-05 17:03:04 +0900

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Movie app 2021
 
 ## react를 사용한 노마드코더 클론코딩.
+
+
+
+### 결과물: https://gbwlxhd97.github.io/movie_app_2021/
---
+++ src/routes/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+function About() {
+  return(
+    <span>
+      About this page: movie pages
+    </span>
+  )
+}
+
+export default About;
